bac0c5d0010bc10864aea821fec7529fb0742d0f
mariam ibrahim <neamamohammed649@gmail.com>
2025-01-30 15:59:01 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyWidget(),
+    ),
+  );
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My first project in VS Code'),
+        ),
+        body: const Center(
+          child: CircleAvatar(
+            radius: 100,
+            backgroundColor: Colors.amber,
+            child: Text(
+              'Eng:Mariam',
+              style: TextStyle(fontSize: 25, color: Colors.blue),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
