c5e44995eae6031eddd137f43814f4219e5ff335
mariam ibrahim <neamamohammed649@gmail.com>
2025-07-18 16:16:48 +0300

    create payment methods list

--- lib/feature/presentation/views/widgets/payment_details_body.dart
+++ lib/feature/presentation/views/widgets/payment_details_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:newproject/feature/presentation/views/widgets/payment_methods_list_view.dart';
 
 class PaymentDetailsBody extends StatelessWidget {
   const PaymentDetailsBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column();
+    return const CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: PaymentMethodsListView(),
+        ),
+      ],
+    );
   }
 }
