309fd67179a2f22f5de6b67f2a501633392751e5
mariam ibrahim <neamamohammed649@gmail.com>
2025-07-20 16:55:17 +0300

    create checkoutRepo and its implementaion

--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:newproject/core/utils/api_keys.dart';
 import 'package:newproject/core/utils/api_service.dart';
 import 'package:newproject/feature/chek_out/data/models/payment_intent_input_model.dart';
@@ -5,15 +6,35 @@ import 'package:newproject/feature/chek_out/data/models/payment_intent_model/pay
 
 class StripeService {
   ApiService apiService = ApiService();
- Future <PaymentIntentModel> createPaymentIntent(
+  Future<PaymentIntentModel> createPaymentIntent(
       PaymentIntentInputModel paymentIntentInputModel) async {
-  final response= await  apiService.post(
+    final response = await apiService.post(
       url: 'https://api.stripe.com/v1/payment_intents',
       body: paymentIntentInputModel.toJson(),
-      token:ApiKeys.secretKey,
+      token: ApiKeys.secretKey,
     );
-    
-      final paymentIntentModel= PaymentIntentModel.fromJson(response.data);
+
+    final paymentIntentModel = PaymentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future initPaymentSheet({required String paymentIntentClintSecret}) async {
+    Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+      paymentIntentClientSecret: paymentIntentClintSecret,
+      merchantDisplayName: 'Mariam',
+    ));
+  }
+
+  Future displayPayMentSheet() async {
+    await Stripe.instance.presentPaymentSheet();
+  }
+
+  Future makePayment(
+      {required PaymentIntentInputModel paymentIntenInputModel}) async {
+    var paymentIntentModel = await createPaymentIntent(paymentIntenInputModel);
+    await initPaymentSheet(
+        paymentIntentClintSecret: paymentIntentModel.clientSecret!);
+    await displayPayMentSheet();
   }
 }
---
+++ lib/feature/chek_out/data/repos/check_out_repo_impl.dart
@@ -0,0 +1,22 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:newproject/core/errors/stripe_failure.dart';
+import 'package:newproject/feature/chek_out/data/models/payment_intent_input_model.dart';
+import 'package:newproject/feature/chek_out/data/repos/check_out_repo.dart';
+
+import '../../../../core/utils/stripe_service.dart';
+
+class CheckOutRepoImpl implements CheckOutRepo {
+  final StripeService stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntenInputModel})async {
+    try{
+    await  stripeService.makePayment(paymentIntenInputModel: paymentIntenInputModel);
+      return  right(null);
+    } on StripeException catch (e) {
+      return left(ServerFailure(errorMessage: e.toString()));
+    } catch (e) {
+      return left(ServerFailure(errorMessage: 'An unexpected error occurred'));
+    }
+  }
+}
